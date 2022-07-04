9e5ab7b8c911993be6bdf70e2db23bbc1f269036
Taslim Owolarafe <85608921+TaslimOwolarafe@users.noreply.github.com>
2022-07-04 08:56:23 +0000

    attempt-1

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.sumOfNumbers = sumOfNumbers;
+  let sum = 0;
+  for (num of arrayOfNumbers) {
+    sum += num;
+  };
+    // return arrayOfNumbers.length;
+  return sum;
+};
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +33,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.countEvenNumbers = countEvenNumbers;
+  let evenCount = 0;
+  for (num of arrayOfNumbers) {
+    if (num % 2 == 0) {
+      evenCount += 1;      
+    };
+  };
+    // return arrayOfNumbers.length;
+  return evenCount;
+};
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
